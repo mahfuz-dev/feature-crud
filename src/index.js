609319d44c19e14f91eb6c5df47e4d29338cf686
mahfuz-dev <mahfuzurrahman.dev@gmail.com>
2021-12-28 00:49:14 +0600

    pushed again for heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
-const ObjectID = require("mongodb").ObjectID;
+const ObjectID = require("mongodb").ObjectId;
 require("dotenv").config();
 
 const app = express();
